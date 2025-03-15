f7ff66985a7da4dce36fd48422b1bf9957df6b1f
oSJSo <129780104+SeoJangSeok@users.noreply.github.com>
2025-03-16 01:45:02 +0900

    [level 0] Title: 카운트 업, Time: 0.00 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -1,5 +1,6 @@
 def solution(start_num, end_num):
-    answer = []
-    for num in range(start_num, end_num + 1):
-        answer.append(num)
-    return answer
+    # answer = []
+    # for num in range(start_num, end_num + 1):
+    #     answer.append(num)
+    # return answer
+    return list(range(start_num, end_num + 1))
