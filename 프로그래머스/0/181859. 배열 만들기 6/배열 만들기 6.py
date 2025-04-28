50360ba346b255ccbe53fe51d21666ad860efe2f
oSJSo <129780104+SeoJangSeok@users.noreply.github.com>
2025-04-28 14:02:21 +0900

    [level 0] Title: 배열 만들기 6, Time: 176.59 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -1,5 +1,4 @@
 def solution(arr):
-    i = 0
     stk = []
     for i in range(len(arr)):
         if stk == []:
@@ -8,5 +7,4 @@ def solution(arr):
             del stk[-1]
         elif stk and stk[-1] != arr[i]:
             stk.append(arr[i])
-        i += 1
     return stk if stk else [-1]
