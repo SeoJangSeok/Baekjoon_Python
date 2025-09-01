894f3b5a63a12a102545e51f278b4952a84e2bf6
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2025-09-01 16:02:55 +0900

    [Bronze III] Title: 윷놀이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,15 @@
+def yut_game(result):
+    if result.count(0) == 1:
+        print('A')
+    elif result.count(0) == 2:
+        print('B')
+    elif result.count(0) == 3:
+        print('C')
+    elif result.count(0) == 4:
+        print('D')
+    else:
+        print('E')
+
+for _ in range(3):
+    result = list(map(int, input().split()))
+    yut_game(result)
