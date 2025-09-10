12528f9def253226fd798a9123a379a8a5a3cff3
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2025-09-10 17:57:27 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.py
@@ -1,7 +1,2 @@
 def solution(numbers, k):
-    if len(numbers) % 2 == 0:
-        numbers = numbers[::2]
-        return numbers[k % len(numbers) - 1]
-    else:
-        numbers = numbers[::2] + numbers[1::2]
-        return numbers[k % len(numbers) - 1]
+    return numbers[2 * (k - 1) % len(numbers)]
