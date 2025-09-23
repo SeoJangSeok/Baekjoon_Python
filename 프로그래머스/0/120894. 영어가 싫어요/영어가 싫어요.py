ede2e006471435ac8f3a9e378110e5207717efdc
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2025-09-23 16:51:39 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    digits = {'zero' : '0', 'one': '1', 'two' : '2', 'three' : '3', 'four' : '4',
+              'five' : '5', 'six' : '6', 'seven' : '7', 'eight' : '8', 'nine' : '9'}
+    for key in digits.keys():
+        numbers = numbers.replace(key, digits[key])
+    return int(numbers)
