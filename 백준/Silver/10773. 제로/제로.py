8f74cdcfcab007981a9c47b5bd86636336a6aee5
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2025-12-24 16:24:53 +0900

    [Silver IV] Title: 제로, Time: 72 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+stack = []
+k = int(input())
+
+for _ in range(k):
+    n = int(input())
+    if n == 0:
+        stack.pop()
+    else:
+        stack.append(n)
+print(sum(stack))
