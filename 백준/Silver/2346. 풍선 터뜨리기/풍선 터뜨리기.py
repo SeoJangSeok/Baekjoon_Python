4519979dc5dc2e80fd4951540bdbb47948e1f7a4
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2026-01-02 23:58:37 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 56 ms, Memory: 34916 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -0,0 +1,17 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+deq = deque(enumerate(map(int, input().split())))
+answer = []
+
+while deq:
+    idx, paper = deq.popleft()
+    answer.append(idx + 1)
+    if paper > 0:
+        deq.rotate(-(paper - 1))
+    elif paper < 0:
+        deq.rotate(-paper)
+
+print(' '.join(map(str, answer)))
