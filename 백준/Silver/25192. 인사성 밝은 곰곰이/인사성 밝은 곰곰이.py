8a755852aff0d59cdef82a620e2033fdc7ae5196
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2026-01-07 17:30:33 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 80 ms, Memory: 39408 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -1,15 +1,18 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 chat_logs = dict()
-cnt = 0
+answer = 0
 
 for _ in range(n):
-    chat = input()
-    
+    chat = input().strip()
+
     if chat == "ENTER":
         chat_logs.clear()
     else:
         if chat not in chat_logs:
             chat_logs[chat] = 1
-            cnt += 1
-            
-print(cnt)
+            answer += 1
+
+print(answer)
