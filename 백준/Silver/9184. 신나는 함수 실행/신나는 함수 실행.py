cc1c5c81d3b302e7324afa1edef6d8375a318a55
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2026-01-22 19:15:44 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 604 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -3,19 +3,19 @@ def w(a, b, c):
         return 1
     if a > 20 or b > 20 or c > 20:
         return w(20, 20, 20)
-    if array[a][b][c]:
-        return array[a][b][c]
-    if a < b < c:
-        array[a][b][c] =  w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
-        return array[a][b][c]
+    if dp[a][b][c]:
+        return dp[a][b][c]
+    if a < b and b < c:
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return dp[a][b][c]
+    else:
+        dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+        return dp[a][b][c]
     
-    array[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
-    return array[a][b][c]
-
-array = [[[0 for _ in range(21)] for _ in range(21)] for _ in range(21)]
+dp = [[[0] * 21 for _ in range(21)] for _ in range(21)]
 
 while True:
     a, b, c = map(int, input().split())
-    if a==-1 and b==-1 and c==-1:
+    if (a, b, c) == (-1, -1, -1):
         break
-    print(f'w({a}, {b}, {c}) = {w(a,b,c)}')
+    print(f'w({a}, {b}, {c}) = {w(a, b, c)}')
