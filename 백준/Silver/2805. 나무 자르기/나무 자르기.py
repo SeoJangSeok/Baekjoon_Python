b465cb74baab0a84fb1a7cca3ac7edfcbd07cae2
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2026-03-08 12:46:34 +0900

    [Silver II] Title: 나무 자르기, Time: 4976 ms, Memory: 143532 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split()) # N: 나무의 수, M: 필요한 길이
+height = list(map(int, input().split())) # 나무의 높이
+
+start, end = 0, max(height)
+
+while start <= end:
+    total = 0
+    mid = (start + end) // 2 # 중간값
+    
+    for h in height:
+        if h > mid:
+            total += h - mid
+    
+    if total < M:
+        end = mid - 1
+    else:
+        start = mid + 1
+print(end)
