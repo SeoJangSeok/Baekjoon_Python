118a73140b05c84dda3f0b2d33a01911e3030388
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2026-03-18 18:34:36 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 552 ms, Memory: 70312 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+def dfs(V):
+    global cnt
+    visited[V] = cnt # 방문하면 몇 번째 순서인지 저장
+    
+    for i in graph[V]:
+        if visited[i] == 0:
+            cnt += 1
+            dfs(i)
+            
+N, M, R = map(int, input().split()) # 정점의 수, 간선의 수, 시작 정점
+visited = [0] * (N+1) # 정점 방문 여부 저장
+graph = [[] for _ in range(N+1)]
+cnt = 1
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
+for i in range(1, N+1):
+    graph[i].sort()
+
+dfs(R)
+
+for i in range(1, N+1):
+    print(visited[i])
