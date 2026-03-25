6c47ac2f948f9d78fd967be138b927eed88bc1f0
S__J___S___ <129780104+SeoJangSeok@users.noreply.github.com>
2026-03-25 17:29:27 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -3,36 +3,34 @@ import sys
 
 input = sys.stdin.readline
 
-N = int(input()) # 지도의 크기 N x N
-
-graph = [list(map(int, input().rstrip())) for _ in range(N)]
-result = []
-count = 0
-
-# 한 점을 기준으로 (위 아래 왼쪽 오른쪽)으로 한 칸 씩 이동할 좌표 설정
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-def dfs(x, y):
-    global count
+def bfs(si, sj):
+    q = deque()
     
-    if x < 0 or x >= N or y < 0 or y >= N:
-        return
-    if graph[x][y] == 1:
-        count += 1
-        graph[x][y] = 0
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs(nx, ny)
+    q.append((si, sj)) # 시작 위치 queue에 삽입
+    visited[si][sj] = 1 # 방문 처리
+    count = 1
+    
+    while q:
+        ci, cj = q.popleft()
+        for dx, dy in ((-1, 0), (1, 0), (0, 1), (0, -1)):
+            ni, nj = ci + dx, cj + dy
+            # 인접 ni, nj가 지도의 범위 내에 있고 방문하지 않았고 집이라면
+            if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] == 0 and map_arr[ni][nj] == 1:
+                q.append((ni, nj))
+                visited[ni][nj] = 1
+                count += 1
+    return count
+        
+
+N = int(input()) # 지도의 크기 N x N
+map_arr = [list(map(int, input().rstrip())) for _ in range(N)] # 지도 생성
+visited = [[0] * N for _ in range(N)] # 방문 표시 배열
+answer = []
 
 for i in range(N):
     for j in range(N):
-        if graph[i][j] == 1:
-            dfs(i, j)
-            result.append(count)
-            count = 0
-
-result.sort()
-
-print(len(result), *result, sep='\n')
+        if map_arr[i][j] == 1 and visited[i][j] == 0: # i행 j열에 집이 있고, 방문하지 않은 상태이면
+            answer.append(bfs(i, j)) # bfs 호출 후 단지 개수 return
+            
+answer.sort()
+print(len(answer), *answer, sep='\n')
